38d5de83d8690d9b61d2af0642b0c5c67851f01d
jacob <jacobja3@gmail.com>
2022-01-22 22:31:28 +0530

    Added delivery service

---
+++ deliveryservice/src/main/java/com/project/delivery/DeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceApplication.class, args);
+	}
+
+}
