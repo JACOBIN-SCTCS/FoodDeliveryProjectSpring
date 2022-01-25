0a2aa1e4246e6756c417d1e4238fe6e3ec6f676d
Kawin M <kawinstark@gmail.com>
2022-01-25 23:37:28 +0530

    Initialised services

--- delivery /src/main/java/com/project/delivery/DeliveryApplication.java
+++ delivery /src/main/java/com/project/delivery/DeliveryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeliveryServiceApplication {
+public class DeliveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeliveryServiceApplication.class, args);
+		SpringApplication.run(DeliveryApplication.class, args);
 	}
 
 }
--- delivery /src/test/java/com/project/delivery/DeliveryApplicationTests.java
+++ delivery /src/test/java/com/project/delivery/DeliveryApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeliveryServiceApplicationTests {
+class DeliveryApplicationTests {
 
 	@Test
 	void contextLoads() {
