f8d87df89f8874fe18ab95be5e2fb2891460eff1
Kawin M <kawinstark@gmail.com>
2022-01-29 15:15:26 +0530

    minor

--- delivery/src/main/java/com/project/delivery/model/DeliveryAgent.java
+++ delivery/src/main/java/com/project/delivery/model/DeliveryAgent.java
@@ -3,9 +3,13 @@ package com.project.delivery.model;
 public class DeliveryAgent {
     
     Long agentId;
+    int status;
 
-    public DeliveryAgent(Long agentId) {
+    
+
+    public DeliveryAgent(Long agentId, int status) {
         this.agentId = agentId;
+        this.status = status;
     }
 
 
