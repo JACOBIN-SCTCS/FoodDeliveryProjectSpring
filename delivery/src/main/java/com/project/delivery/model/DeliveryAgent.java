d67278e3e6b7c5889342475a77ad3b00756e592e
Kawin M <kawinstark@gmail.com>
2022-01-29 06:18:53 +0530

    Delivery Service fix

---
+++ delivery/src/main/java/com/project/delivery/model/DeliveryAgent.java
@@ -0,0 +1,12 @@
+package com.project.delivery.model;
+
+public class DeliveryAgent {
+    
+    Long agentId;
+
+    public DeliveryAgent(Long agentId) {
+        this.agentId = agentId;
+    }
+
+
+}
