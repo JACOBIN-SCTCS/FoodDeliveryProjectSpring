3f950f730282d7888ef4e011fdeaa55a00ea5b63
jacob <jacobja3@gmail.com>
2022-02-26 22:24:26 +0530

    Added key value state and agent entity

--- Delivery/src/main/java/com/project/delivery/entities/AgentEntity.java
+++ Delivery/src/main/java/com/project/delivery/entities/AgentEntity.java
@@ -1,6 +1,43 @@
-package main.java.com.project.delivery.entities;
+package com.project.delivery.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 
 @Entity
 public class AgentEntity {
     
+    @Id
+    @Column(name = "agentId")
+    int agentId;
+    @Column(name = "status")
+    int status;
+
+    AgentEntity()
+    {
+
+    }
+    AgentEntity(int agentId, int status)
+    {
+        this.agentId = agentId;
+        this.status = status;
+    }
+    public int getAgentId() {
+        return this.agentId;
+    }
+
+    public void setAgentId(int agentId) {
+        this.agentId = agentId;
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+
 }
--- Delivery/src/main/java/com/project/delivery/entities/CurrentState.java
+++ Delivery/src/main/java/com/project/delivery/entities/CurrentState.java
@@ -1,5 +1,33 @@
-package main.java.com.project.delivery.entities;
+package com.project.delivery.entities;
 
-public class CurrentState {
-    
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CurrentState 
+{
+    @Id
+    @Column(name = "key")
+    int key;
+    @Column(name = "value")
+    int value;
+
+	public int getKey() {
+		return this.key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+
 }
