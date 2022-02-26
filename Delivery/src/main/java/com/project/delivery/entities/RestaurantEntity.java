7d72c26331c67f9f89cd73505f28a799e3e620e5
jacob <jacobja3@gmail.com>
2022-02-26 22:32:49 +0530

    Added ResturantItem entity

--- Delivery/src/main/java/com/project/delivery/entities/RestaurantEntity.java
+++ Delivery/src/main/java/com/project/delivery/entities/RestaurantEntity.java
@@ -1,5 +1,68 @@
-package main.java.com.project.delivery.entities;
+package com.project.delivery.entities;
 
-public class RestaurantEntity {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+public class RestaurantEntity 
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idgen")
+    @SequenceGenerator(name = "idgen", initialValue = 1, allocationSize = 1)    
+    int id;
+
+    @Column(name = "restId")
+    int restId;
+    @Column(name = "itemId")
+    int itemId;
+    @Column(name = "price")
+    int price;
+
+    public RestaurantEntity()
+    {
+
+    }
+    public RestaurantEntity(int restId, int itemId, int price)
+    {
+        this.restId = restId;
+        this.itemId = itemId;
+        this.price = price;
+    }
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRestId() {
+        return this.restId;
+    }
+
+    public void setRestId(int restId) {
+        this.restId = restId;
+    }
+
+    public int getItemId() {
+        return this.itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
     
 }
